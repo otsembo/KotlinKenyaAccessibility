4c7ebd762f8dd1eeb13b2ec6161dc2846c44237d
Ian Okumu <okumu.otsembo@gmail.com>
2022-04-22 07:59:02 +0300

    Compose: checkbox and text

--- App-Compose/app/src/main/java/com/example/accessibilitydemo_compose/MainActivity.kt
+++ App-Compose/app/src/main/java/com/example/accessibilitydemo_compose/MainActivity.kt
@@ -3,17 +3,19 @@ package com.example.accessibilitydemo_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.TopAppBar
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -36,6 +38,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppPage() {
 
+    var username by remember{ mutableStateOf("")}
+    var password by remember{ mutableStateOf("")}
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -57,8 +62,8 @@ fun AppPage() {
                 .padding(top = 16.dp, start = 16.dp, end = 16.dp))
             
             
-            OutlinedTextField(value = "",
-                onValueChange = {},
+            OutlinedTextField(value = username,
+                onValueChange = {username = it},
                 label = {
                 Text(text = stringResource(id = R.string.username),)
             },
@@ -66,12 +71,13 @@ fun AppPage() {
                     .fillMaxWidth()
                     .padding(start = 16.dp, end = 16.dp, top = 16.dp))
 
-            OutlinedTextField(value = "",
-                onValueChange = {},
+            OutlinedTextField(value = password,
+                onValueChange = {password = it},
                 label = {
                     Text(text = stringResource(id = R.string.password),)
                 },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = PasswordVisualTransformation(),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 16.dp, end = 16.dp, top = 16.dp),
@@ -122,15 +128,21 @@ fun MyImage(modifier: Modifier) {
 
 @Composable
 @ExperimentalMaterial3Api
-fun MyCheckBox(modifier: Modifier, text:String = "") {
+fun MyCheckBox(modifier: Modifier, text:String = "",) {
+
+    var isChecked by remember {
+        mutableStateOf(false)
+    }
 
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
 
-        Checkbox(checked = false,
-            onCheckedChange = {},
+        Checkbox(checked = isChecked,
+            onCheckedChange = {isChecked = it},
         )
 
-        Text(text = text, )
+        Text(text = text, modifier = Modifier
+            .clickable(enabled = true)
+            { isChecked =! isChecked })
 
     }
 
